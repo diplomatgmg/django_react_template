a13057a62999323abf47795be90d01f5d1422e48
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-02 17:07:51 +0300

    Updated startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -6,5 +6,9 @@ echo "Migrate database..."
 python manage.py migrate
 echo "Database migrated"
 
+echo "Loading fixtures..."
+python manage.py loaddata __fixtures__/*.json
+echo "Fixtures loaded"
+
 echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000
