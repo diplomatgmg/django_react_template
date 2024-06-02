16e22e0f53cddeb1d2743c6e3f8a2f4bd90c6b35
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-02 16:26:31 +0300

    Added django rest framework

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Libraries
+    "rest_framework"
+    # Custom apps
 ]
 
 MIDDLEWARE = [
