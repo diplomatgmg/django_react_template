020c80abdf2612cae95a48684dd3e559c7c4eeff
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-02 17:08:10 +0300

    Added endpoint /api/products

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
