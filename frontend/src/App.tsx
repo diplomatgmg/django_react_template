1d116039007426d092d9f7dd83c9c7f5143bad17
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-25 02:44:24 +0300

    Add error handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,7 @@ import { useExampleEndpointQuery } from "./store/exampleApi.ts"
 
 function App() {
   const [count, setCount] = useState(0)
-  const { data, isLoading } = useExampleEndpointQuery()
-
-  if (isLoading) return <h1>Loading...</h1>
+  const { data, isError } = useExampleEndpointQuery()
 
   return (
     <>
@@ -28,7 +26,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <ul>{data?.map(({ id, name }) => <li key={id}>{name}</li>)}</ul>
+        {isError && <h1>Data fetch error</h1>}
+        {!isError && (
+          <ul>{data?.map(({ id, name }) => <li key={id}>{name}</li>)}</ul>
+        )}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
