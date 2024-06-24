2c6e56d566679c4b3fbed4bc53fef04030fb404a
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-24 19:44:27 +0300

    Removed app products

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("apps.products.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
